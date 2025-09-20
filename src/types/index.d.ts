41f564ea06ffabfa6aaf31b3b0e795b0d2ce0a3a
Kaustav Banerjee <kaustav123.kb@gmail.com>
2025-09-20 18:36:03 +0530

    rafactored type definitions

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -2,3 +2,6 @@ export * from './movie';
 export * from './user';
 export * from './store';
 export * from './api';
+
+// Re-export store types from the actual store
+export type { RootState, AppDispatch } from '../store';
